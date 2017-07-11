420568565e17ad6ffb93c32679cfa359094aee2d
nardsqq <nardparagas@gmail.com>
2017-07-12 01:04:06 +0800

    Customized buttons

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,6 +16,8 @@
     {{-- <link rel="stylesheet" href="{{ asset('/DataTables/datatables.min.css') }}"/> --}}
     <link rel="stylesheet" href="{{ asset('/css/custom/wagon-style.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/custom/flatify.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/custom/btn-round.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/custom/btn-color.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/animate/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('/toastr-master/build/toastr.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/parsley.css') }}">
