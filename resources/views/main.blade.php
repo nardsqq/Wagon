285dd6d9a39f1384032c7e6aeed82aee57b2e49e
nardsqq <nardparagas@gmail.com>
2017-07-13 14:02:22 +0800

    Added fa-spin

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -42,7 +42,7 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hello, Admin <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="#"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Account</a></li>
+                <li><a href="#"><i class="fa fa-cog fa-fw fa-spin" aria-hidden="true"></i>&nbsp; Account</a></li>
                 <li><a href="#"><i class="fa fa-folder-open fa-fw" aria-hidden="true"></i>&nbsp; System Settings</a></li>
                 <li role="role" class="divider"></li>
                 <li><a href="#"><i class="fa fa-power-off fa-fw" aria-hidden="true"></i>&nbsp; Sign Out</a></li>
