0ae46d1d60d9729bd0afdf61e85be6b4a1a48117
nardsqq <nardparagas@gmail.com>
2017-07-15 01:36:28 +0800

    Changed file folder name

---
+++ resources/views/transactions/salesorder/index.blade.php
@@ -0,0 +1,63 @@
+@extends('main')
+@section('content')
+
+ <header id="header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10">
+          <h1><i class="fa fa-bar-chart" aria-hidden="true"></i> Transactions</h1>
+        </div>
+        <div class="col-md-2">
+
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div class="container fadeIn">
+    @include('transactions.purchaseorder.nav')
+  </div>
+
+  <section id="breadcrumb">
+    <div class="container animated fadeIn">
+      <ol class="breadcrumb">
+        <li>Admin</li>
+        <li>Transactions</li>
+        <li>Sales Orders</li>
+      </ol>
+    </div>
+  </section>
+
+  <section id="main">
+    <div class="container animated fadeIn">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="alert alert-success alert-white rounded">
+            <div class="icon">
+              <i class="fa fa-info-circle"></i>
+            </div>
+            <strong> Manage your <i>Client's Sales Orders</i> here.</strong>
+            <br>
+            <small>Add and manage <i><b>Sales Order</b></i>.</small>
+          </div>  
+          <div class="panel panel-default">
+            <div class="panel-heading clearfix">
+              <div class="btn-group pull-right">
+                <button type="button" id="btn-add" class="btn btn-success"><i class="fa fa-plus-square"></i>&nbsp;New Sales Order</button>
+              </div>
+              <div class="panel-title">
+                <h4>Sales Orders</h4>
+              </div>
+            </div>
+            <div class="panel-body">
+              <div id="table-container">
+                @include('transactions.purchaseorder.table')
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+  </section>
+@endsection
