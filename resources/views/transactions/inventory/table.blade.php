91ef7b3a5bb61075ee3f8cea7c0297d4befded68
nardsqq <nardparagas@gmail.com>
2017-10-18 01:39:59 +0800

    [TRANSACTIONS] Stock Control WIP

---
+++ resources/views/transactions/inventory/table.blade.php
@@ -0,0 +1,25 @@
+<table id="dataTable" class="table table-bordered table-hover" style="visibility: hidden;" width="100%">
+  <thead>
+    <tr>
+      <th>Role</th>
+      <th>Regulation</th>
+      <th>Full Name</th>
+      <th>Mobile Number</th>
+      <th class="text-center">Actions</th>
+    </tr>
+  </thead>
+  <tbody id="prod-list">
+    @foreach ($personnels as $personnel)
+      <tr id="id{{ $personnel->intPersID }}">
+        <td>{{ $personnel->roles->strRoleName }}</td>
+        <td>{{ $personnel->strPersEmpType }}</td>
+        <td>{{ $personnel->strPersLName }}, {{ $personnel->strPersFName }} {{ $personnel->strPersMName }}</td>
+        <td>{{ $personnel->strPersMobNo }}</td>
+        <td class="text-center">
+            <button class="btn btn-info btn-sm btn-detail open-modal" value="{{ $personnel->intPersID }}"><i class='fa fa-edit'></i>&nbsp; Edit</button>
+            <button class="btn btn-danger btn-sm btn-delete" value="{{ $personnel->intPersID }}"><i class='fa fa-trash-o'></i>&nbsp; Delete</button>
+        </td>
+      </tr>
+    @endforeach
+  </tbody>
+</table>
