187b56895960004b89395bb49dade2ec6575fbdc
golo <amielgolosinda@ymail.com>
2017-10-21 06:27:56 +0800

    Update quotation Report

--- resources/views/pdf/quotation-pdf.blade.php
+++ resources/views/pdf/quotation-pdf.blade.php
@@ -1,3 +1,4 @@
+@php $product_total = 0;$service_total = 0; @endphp
 <!DOCTYPE html>
 <html>
 <head>
@@ -98,11 +99,71 @@
     <br>
     <div>
         <h2>Products or Services Quoted:</h2>
-        <div class="border">
-            <h4 class="text-center">
-                
-            </h4>
-        </div>
+        @if($quote->products->count() > 0)
+            <div class="border">
+                <h4 class="text-center">
+                    Products
+                </h4>
+            </div>
+            <table class="table table-hover table-condensed table-bordered table-responsive m-t-20" style="width: 100%">
+              <thead>
+                <tr>
+                  <th class="text-center">#</th>
+                  <th class="text-center">Product</th>
+                  <th class="text-center">Quantity</th>
+                  <th class="text-center">Unit Price</th>
+                  <th class="text-center">Total Cost</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($quote->products as $product)
+                <tr>
+                  <td class="text-center">{{ $loop->iteration }}</td>
+                  <td class="text-center">
+                    {{ $product->variant->full_detail }}
+                  </td>
+                  <td class="text-center">{{ $product->intQuotDPQuantity }}</td>
+                  <td class="text-center">{{ $product->decQuotDPUnitPrice }}</td>
+                  <td class="text-center">{{ $product_total += $product->intQuotDPQuantity * $product->decQuotDPUnitPrice }}</td>
+                </tr>
+                @endforeach
+              </tbody>
+              </table>
+              <h3 style="text-align: right">Products Total: {{ $product_total }}</h3>
+            @endif
+
+            @if($quote->services->count() > 0)
+            <div class="border">
+                <h4 class="text-center">
+                    Services
+                </h4>
+            </div>
+            <table class="table table-hover table-condensed table-bordered table-responsive m-t-20" style="width: 100%">
+              <thead>
+                <tr>
+                    <th class="text-center">#</th>
+                    <th class="text-center">Service</th>
+                    <th class="text-center">Service Fee</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($quote->services as $service)
+                <tr>
+                  <td class="text-center">{{ $loop->iteration }}</td>
+                  <td class="text-center">
+                    {{ $service->strServAreaName }}
+                  </td>
+                  <td class="text-center">{{ $service->intQuotDPQuantity }}</td>
+                  <td class="text-center">{{ $service_total +=  $service->decQuotDSPrice }}</td>
+                </tr>
+                @endforeach
+              </tbody>
+              </table>
+              <h3 style="text-align: right">Services Total: {{ $service_total }}</h3>
+            @endif
+
+            
+              <h2 style="text-align: right">Total: {{ $service_total + $product_total }}</h2>
     </div>
     <div class="footer">
     </div>
