90acdaf7f4893ba4fe07c1556ace1a8d5f0ec443
nardsqq <nardparagas@gmail.com>
2017-10-20 23:31:36 +0800

    [DB]

--- resources/views/transactions/receive-items/modal.blade.php
+++ resources/views/transactions/receive-items/modal.blade.php
@@ -46,29 +46,57 @@
   </div>
 </div>
 
-<div class="modal fade" id="edit_mode" role="dialog">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header modal-header-info" id="mode-modal-header-info">
-        <h4 id="title">Edit Mode Of Payment Record</h4>
-      </div>
-      <div class="modal-body">
-        <form id="formEditMode">
+<!-- modal for new and update -->
+  <div class="modal fade" id="modalMultipleChoice">
+    <div class="modal-dialog modal-70">
+      <div class="modal-content">
+        <!-- modal header -->
+        <div class="modal-header">
+          <button class="close" data-dismiss="modal">&times;</button>
+          <h3 id="modalTitle">XXX</h3>
+        </div>
+        <!-- modal body -->
+        <div class="modal-body">
+          <form id="formMultipleChoice" data-parsley-validate>
+            <div class="form-group">
+              <label>Question <span class="asterisk-red">*</span></label>
+              <textarea id="inputQuestion" rows="3" class="form-control" required></textarea>
+            </div>
+          </form>
+          <form id="formChoice" data-parsley-validate>
+            <div class="form-group">
+              <label>Choice <span class="asterisk-red">*</span></label>
+              <input type="text" class="form-control" id="inputChoice" required><br>
+              <label><input type="checkbox" name="cbCorrect" id="cbCorrect"> CORRECT</label>
+              <div class="pull-right">
+                <button class="btn btn-primary btn-block" id="btnChoiceAdd">ADD CHOICE</button>
+              </div>
+            </div><hr>
+            <div class="form-group">
+              <table id="tblChoice" class="table table-striped table-bordered">
+                <thead>
+                  <th>Choice</th>
+                  <th style="text-align: center;">Answer</th>
+                  <th style="text-align: center;">Action</th>
+                </thead>
+                <tbody id="variant-list"></tbody>
+              </table>
+            </div>
+          </form>
+        </div>
+        <!-- modal footer -->
+        <div class="modal-footer">
           <div class="form-group">
-            {!! Form::label('strMODName', 'Mode of Payment') !!}
-            {!! Form::text('strMODName', null, ['id' => 'strModName', 'class' => 'form-control']) !!}
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="modal-footer">
+              <button class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
+              <button id="btnSave" value="New" class="modal-btn btn btn-success pull-right">Submit</button>
+              <input type="hidden" id="link_id" name="link_id" value="0">
+            </div>
           </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
-        <button id="btn-update" value="update" class="modal-btn btn btn-info pull-right">Update</button>
-        <input type="hidden" id="link_id" name="link_id" value="0">
+        </div>
       </div>
     </div>
   </div>
-</div>
 
 <div class="modal fade" id="del_mode">
   <div class="modal-dialog">
