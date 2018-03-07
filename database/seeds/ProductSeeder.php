26bf39264341668512b8f6159c1242c3d11b818a
amiel <amielgolosinda@ymail.com>
2018-03-07 20:21:03 +0800

    Update product seeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -17,37 +17,88 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        $product = Product::firstOrCreate([
-            'str_product_name' => 'Product 1'
-        ]);
+        $products = [
+            'Genset' => [
+                'Perkin' => [
+                    'fuel type' => 'Diesel',
+                    'frequency' => '60Hz',
+                    'engine speed' => '1500 RPM',
+                    'voltage' => '400/230v'
+                ],
+                'Cummins' => [
+                    'fuel type' => 'Diesel',
+                    'frequency' => '60Hz',
+                    'engine speed' => '1500 RPM',
+                    'voltage' => '400/230v'
+                ],
+                'Yanmar' => [
+                    'fuel type' => 'Diesel',
+                    'frequency' => '60Hz',
+                    'engine speed' => '1500 RPM',
+                    'voltage' => '400/230v'
+                ]
+            ],
+            'Power Pump' => [
+                'PP-12V-17AH' => [
+                    'fuel type' => 'Diesel',
+                    'battery'=> '12V-17AH'
+                ],
+                'PP-12V-22AH' => [
+                    'fuel type' => 'Diesel',
+                    'battery'=> '12V-22AH'
+                ],
+                'PP-12V-30AH' => [
+                    'fuel type' => 'Diesel',
+                    'battery'=> '12V-30AH'
+                ],
+                'PP-12V-36AH' => [
+                    'fuel type' => 'Diesel',
+                    'battery'=> '12V-36AH'
+                ]
+            ],
+            'Concrete Cutter' => [
+                'Diesel' => [
+                    'fuel type' => 'Diesel',
+                    'driving' => 'Manual Push',
+                    'depth adjustment' => 'Handle Rotation'
+                ],
+                'Petrol' => [
+                    'fuel type' => 'Petrol',
+                    'driving' => 'Manual Push',
+                    'depth adjustment' => 'Handle Rotation'
+                ]
+            ]
 
-        $variant = Variant::firstOrCreate([
-            'int_prod_id_fk' => $product->int_product_id,
-            'str_var_name' => 'Variant 1'
-        ]);
+        ];
 
-        $variant->prices()->firstOrCreate([
-            'dbl_price' => 1000.00
-        ]);
+        foreach($products as $prod_name => $variants){
+            $product = Product::firstOrCreate([
+                'str_product_name' => $prod_name
+            ]);
 
-        $variant->stocks()->firstOrCreate([
-            'int_quantity' => 100
-        ]);
+            foreach($variants as $var_name => $specs){
+                $variant = Variant::firstOrCreate([
+                    'int_prod_id_fk' => $product->int_product_id,
+                    'str_var_name' => $var_name
+                ]);
+        
+                $variant->prices()->firstOrCreate([
+                    'dbl_price' => 1000.00
+                ]);
+        
+                $variant->stocks()->firstOrCreate([
+                    'int_quantity' => 100
+                ]);
 
-        $variant->specs()->firstOrCreate([
-            'int_spec_pa_id_fk' => $product->prod_attribs()->firstOrCreate([
-                'int_pa_attrib_id_fk' => Attribute::firstOrCreate(['str_attrib_name' => 'attribute-2'])->int_attrib_id
-            ])->int_prod_attrib_id,
-            'str_spec_constant' => 'value-2'
-        ]);
-
-        $variant->specs()->firstOrCreate([
-            'int_spec_pa_id_fk' => $product->prod_attribs()->firstOrCreate([
-                'int_pa_attrib_id_fk' => Attribute::firstOrCreate(['str_attrib_name' => 'attribute-1'])->int_attrib_id
-            ])->int_prod_attrib_id,
-            'str_spec_constant' => 'value-1'
-        ]);
-    
-                
+                foreach($specs as $attrib_name => $attrib_value){
+                    $variant->specs()->firstOrCreate([
+                        'int_spec_pa_id_fk' => $product->prod_attribs()->firstOrCreate([
+                            'int_pa_attrib_id_fk' => Attribute::firstOrCreate(['str_attrib_name' => $attrib_name])->int_attrib_id
+                        ])->int_prod_attrib_id,
+                        'str_spec_constant' => $attrib_value
+                    ]);
+                }
+            }
+        }                
     }
 }
