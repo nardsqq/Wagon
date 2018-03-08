7d121157c263e1fb2d3f9442c0ee506fc570c023
amiel <amielgolosinda@ymail.com>
2018-03-09 00:30:53 +0800

    update order details

--- resources/views/transactions/order/form/order-details.blade.php
+++ resources/views/transactions/order/form/order-details.blade.php
@@ -3,34 +3,6 @@
     {{ Form::text('order_num', null,['class'=>'form-control', 'v-model'=>'order_num[order_type]', 'placeholder'=>'Purchase Order Number','readonly'=>true]) }}
 </div>
 <hr>
-
-<div class="col-xs-12  m-t-10">
-    <label class="control-label" required>Delivery Type</label>
-    <div>
-        <label>
-            {!! Form::radio('delivery_type', 0, true, ['v-model'=>'delivery_option']); !!}
-            Delivery
-        </label>
-        
-        <label>
-            {!! Form::radio('delivery_type', 1, false, ['v-model'=>'delivery_option']); !!}
-            Pick-up
-        </label>
-    </div>
-</div>
-
-<div class="col-xs-12  m-t-10" v-if="delivery_option == 0">
-    <label for="delivery_location" required>Delivery Location</label>
-    {{ Form::textarea('delivery_location', null,['class'=>'form-control', 'rows'=>4, ':value'=>'selected_client.txt_client_address']) }}
-</div>
-
-<div class="col-xs-12  m-t-10" v-if="delivery_option == 0">
-    <label for="landmark">Landmark</label>
-    {{ Form::text('landmark', null, ['class'=>'form-control', ':value'=>'selected_client.str_client_landmark']) }}
-</div>
-
-<hr>
-
 <div class="col-xs-12  m-t-10">
     <label class="control-label" required>Order Type</label>
     <div>
@@ -44,4 +16,31 @@
             Service
         </label>
     </div>
+</div>
+
+<hr>
+
+<div class="col-xs-12  m-t-10">
+    <label class="control-label" required>Delivery Type</label>
+    <div>
+        <label>
+            {!! Form::radio('delivery_type', 0, true, ['v-model'=>'delivery_option', ':disabled'=>'order_type==1']); !!}
+            Delivery
+        </label>
+        
+        <label>
+            {!! Form::radio('delivery_type', 1, false, ['v-model'=>'delivery_option', ':disabled'=>'order_type==1']); !!}
+            Pick-up
+        </label>
+    </div>
+</div>
+
+<div class="col-xs-12  m-t-10" v-if="delivery_option == 0">
+    <label for="delivery_location" required>Delivery Location</label>
+    {{ Form::textarea('delivery_location', null,['class'=>'form-control', 'rows'=>4, ':value'=>'selected_client.txt_client_address']) }}
+</div>
+
+<div class="col-xs-12  m-t-10" v-if="delivery_option == 0">
+    <label for="landmark">Landmark</label>
+    {{ Form::text('landmark', null, ['class'=>'form-control', ':value'=>'selected_client.str_client_landmark']) }}
 </div>
