bdea10ca6430298a674083c89ad649474fd77a89
amiel <amielgolosinda@ymail.com>
2018-03-09 03:25:23 +0800

    set delivery details

--- public/js/ajax/transactions/delivery-ajax.js
+++ public/js/ajax/transactions/delivery-ajax.js
@@ -18,56 +18,58 @@ $(document).ready(function() {
 	$(document).on('click', '.open-modal', function() {
 	  var id = $(this).val();
 	  delivery_form.find('#int_delivery_id').val(id);
+	  delivery_form.find('#dat_delivery_date').val($(this).attr('data-date'));
+	  delivery_form.find('#int_personnel_id').val($(this).attr('data-personnel'));
 	  $('#set-modal').modal('show');
 	});
   
-	$('#btn-delivery_form').on('submit', function(e) {
-	  e.preventDefault();
+	$(document).on('submit', '#delivery_form', function(e) {
+	  	e.preventDefault();
 		submitForm('#delivery_form');
 	});
 
+	
+	function submitForm(form_name = ''){
+		var form = $(form_name),
+			form_data = form.serialize();
+
+		$.ajax({  
+			type: form.attr('method'),  
+			url: form.attr('action'),  
+			data: form_data,  
+			dataType: 'json',
+			success: function(data) {  
+				if(data.alert=='success'){  
+					$(form_name).trigger('reset');
+					$('#set-modal').modal('hide');
+					loadTable();
+					toastr.success(data.message, data.title?data.title:'');
+				} else {
+					alert('Error');
+				}
+			},  
+			error: function (data) {  
+				var message = data.responseJSON.message ? data.responseJSON.message : 'Error!';
+				var errors = data.responseJSON.message ? '' : data.responseJSON;  
+				var error_message = '';
+				for(i in errors){  
+					error_message += errors[i] + '\n';  
+				}  
+				alert(error_message);
+			}
+		});
+	}
+
 	function loadTable() {
 		$.ajax({
-		  type: 'get',
-		  url: url + "-table",
-		  dataType: 'html',
-		  success:function(data) {
-			console.log(url);
-			console.log(data);
+		type: 'get',
+		url: url + "-table",
+		dataType: 'html',
+		success:function(data) {
 			$('#dataTable').html(data).fadeIn(300);
 			// $('#dataTable').dataTable();
-		  }
+		}
 		})
-	  } // function loadTable() {}
+	} // function loadTable() {}
 	  
 	}); // $(document).ready(function() {});
-
-function submitForm(form_name = ''){
-	var form = $(form_name),
-		form_data = form.serialize();
-
-	$.ajax({  
-		type: form.attr('method'),  
-		url: form.attr('action'),  
-		data: form_data,  
-		dataType: 'json',
-		success: function(data) {  
-			if(data.alert=='success'){  
-				$(form_name).trigger('reset');
-				$('#set-modal').modal('hide');
-				loadTable();
-			} else {
-				alert('Error');
-			}
-		},  
-		error: function (data) {  
-			var message = data.responseJSON.message ? data.responseJSON.message : 'Error!';
-			var errors = data.responseJSON.message ? '' : data.responseJSON;  
-			var error_message = '';
-			for(i in errors){  
-				error_message += errors[i] + '\n';  
-			}  
-			alert(error_message);
-		}
-	});
-}
--- resources/views/transactions/delivery/modal.blade.php
+++ resources/views/transactions/delivery/modal.blade.php
@@ -6,20 +6,21 @@
         <h4 id="title">Set Delivery Details</h4>
       </div>
       <div class="modal-body">
-
+        <div class="col-xs-12">
           {!!Form::hidden('int_delivery_id', null, array('id' => 'int_delivery_id')); !!}
           <div class="form-group">
             {!! Form::label('dat_delivery_date', 'Date of Delivery') !!}
-            <input type="date" name="dat_delivery_date" class="form-control">
+            <input type="date" name="dat_delivery_date" id="dat_delivery_date" class="form-control">
           </div>
           <div class="form-group">
             {!! Form::label('personnel', 'Personnel in Charge') !!}
-            <select name="int_personnel_id" id="personnel" class="form-control">
+            <select name="int_personnel_id" id="int_personnel_id" class="form-control">
               @foreach($personnels as $personnel)
                 <option value="{{ $personnel->int_personnel_id }}">{{$personnel->name}}</option>
               @endforeach
             </select>
           </div>
+        </div>
       </div>
       <div class="modal-footer">
         <button class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
