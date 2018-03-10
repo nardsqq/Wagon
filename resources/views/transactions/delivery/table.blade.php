e82d36885849ee7d1446a64d013d85c42aeed293
amiel <amielgolosinda@ymail.com>
2018-03-10 08:30:06 +0800

    hide delivery receipt

--- resources/views/transactions/delivery/table.blade.php
+++ resources/views/transactions/delivery/table.blade.php
@@ -20,13 +20,14 @@
         <span class="label label-{{ $delivery->current_status->value->class }}"><i class="fa fa-fw {{ $delivery->current_status->value->icon }}" aria-hidden="true"></i>&nbsp; {{ $delivery->current_status->str_status }}</span>
       </td>
       <td class="text-center">
-          <a target="_blank" href="{{ route('delivery.receipt', $delivery->int_delivery_id) }}"class="btn btn-details btn-xs btn-default"><i class="fa fa-circle-o fa-fw"></i>&nbsp; View Delivery Receipt</a>
           @if($delivery->current_status->str_status == 'New')
           <button class="btn btn-primary btn-xs btn-detail open-modal" value="{{ $delivery->int_delivery_id }}"
               data-date="{{ $delivery->dat_delivery_date }}"
               data-personnel="{{ $delivery->int_del_personnel_id_fk }}"
             ><i class='fa fa-calendar'></i>&nbsp; Set Delivery Details</button>
           @elseif($delivery->current_status->str_status == 'On delivery')
+          
+          <a target="_blank" href="{{ route('delivery.receipt', $delivery->int_delivery_id) }}"class="btn btn-details btn-xs btn-default"><i class="fa fa-circle-o fa-fw"></i>&nbsp; View Delivery Receipt</a>
           <a href="{{ route('delivery.complete', $delivery->int_delivery_id) }}"class="btn btn-success btn-xs btn-detail open-complete-modal" value="{{ $delivery->int_delivery_id }}"
             ><i class='fa fa-check'></i>&nbsp; Delivery Completed</a>
           @endif
